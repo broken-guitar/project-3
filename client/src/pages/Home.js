13aacea6fc1612fb2ba606702d3f3df6aa3232e5
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-06-23 19:40:22 -0700

    start with categicon, item and sectiontitle icons (incomplete) and display without props on home page to view

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Categicon from "../components/categicon/categicon.js";
+import Sectiontitle from "../components/sectiontitle/sectiontitle.js";
 
 export default function Home() {
-    return(
-        <h1>Placeholder: We're at the home page</h1>
+    return (
+        <>
+            <h1>Placeholder: We're at the home page</h1>
+            <Categicon></Categicon>
+            <Sectiontitle></Sectiontitle>
+        </>
     )
 }
