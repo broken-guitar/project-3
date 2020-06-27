ee4a0514a0e980540213f23c5134098a32d497fe
David Flores <drfloresj@gmail.com>
2020-06-26 19:11:37 -0700

    added 'graceful' shutdown function to server.js

--- server.js
+++ server.js
@@ -39,3 +39,30 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+      console.log('Mongoose disconnected through ' + msg);
+      callback();
+    });
+  };
+  // For nodemon restarts
+  process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+      process.kill(process.pid, 'SIGUSR2');
+    });
+  });
+  // For app termination
+  process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function () {
+      process.exit(0);
+    });
+  });
+  // For Heroku app termination
+  process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function () {
+      process.exit(0);
+    });
+  });
